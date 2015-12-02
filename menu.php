0a2737ed43d1388f49a8edecbd8a6d2ee7015522
cicerohigor <higorgomesousa@gmail.com>
2015-12-02 18:11:45 -0400

    ATUALIZADO
    # Conflicts:
    #	menu.php

--- menu.php
+++ menu.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html  >
 <html >
 <head>
+
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <title> Carrinho de Compras</title>
  <link rel="stylesheet" type="text/css" href="css/estilo1.css">
@@ -20,12 +21,27 @@
 <?php
       require("conexao.php");
        
-      $sql = "SELECT * FROM promocao where CATEGORIA ='Promocao'";
+      $sql = "SELECT * FROM produtos where CATEGORIA ='Promocao'";
       $qr = mysql_query($sql) or die(mysql_error());
       while($ln = mysql_fetch_assoc($qr)){
          echo '<h2 align="center">'.$ln['nome'].'</h2> <br />';
          echo '<h4 align="center">PRECO :  '.$ln['preco']. '.'.'<br />';
-	 		 echo '<img src='.$ln["imagem"].' >';
+	 		 echo '<img src='.$ln["imagem"].' id="imagem">';
+         echo '<br><a href="carrinho1.php?acao=add&id='.$ln['idCodigo'].'">COMPRAR BRINQUEDO</a>';
+         echo '<br /><hr />';
+      }
+?>
+
+<p><img src="figura/logo22.png" ></p>
+<?php
+      require("conexao.php");
+       
+      $sql = "SELECT * FROM produtos where CATEGORIA ='Mais Vendidos'";
+      $qr = mysql_query($sql) or die(mysql_error());
+      while($ln = mysql_fetch_assoc($qr)){
+         echo '<h2 align="center">'.$ln['nome'].'</h2> <br />';
+         echo '<h4 align="center">PRECO :  '.$ln['preco']. '.'.'<br />';
+	 		 echo '<img src='.$ln["imagem"].' id="imagem" >';
          echo '<br><a href="carrinho1.php?acao=add&id='.$ln['idCodigo'].'">COMPRAR BRINQUEDO</a>';
          echo '<br /><hr />';
       }
